246fdd9c15992454d7b0532e97bbca2fa1f663ac
khali16 <linertk@gmail.com>
2021-06-18 14:14:30 +0200

    authentication

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import styles from "./LoginForm.module.css";
 import useInput from "../../hooks//useInput";
 import {
   hasAtLeastFiveLetters,
   passwordValidator,
 } from "../../utils/validation";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 const LoginForm = () => {
+  const authContext = useContext(AuthContext);
+  const history = useHistory();
+
   const {
     value: enteredLogin,
     isValid: loginIsValid,
@@ -28,13 +33,47 @@ const LoginForm = () => {
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (!loginIsValid && !passwordIsValid) {
+    if (!loginIsValid) {
       return;
     }
     console.log(enteredLogin, enteredPassword);
 
-    loginReset();
-    passwordReset();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyApa1WrZ97H3bjYtU-rlQzjOoFs_9HT7PI",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredLogin,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then(() => {
+            let errorMessage = "Authentication failed!";
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        // @ts-ignore
+        authContext.login(data.idToken, expirationTime.toISOString());
+        history.replace("/");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const loginInputStyles = loginHasError ? styles.invalid : "";
